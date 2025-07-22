2fff54c2b1a6d2bcb601cb45764755b1d1db767e
Darko Todoric <darko.todoric013@gmail.com>
2025-07-22 15:24:53 +0200

    clean crontab

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,3 @@ sudo -u www-data php bin/console cache:clear;
 
 # Update database
 sudo -u www-data php bin/console doctrine:schema:update --force;
-
-# Update crontab
-sudo -u www-data crontab < config/crontab.sh;
-sudo crontab < config/crontab-root.sh;
